c18aaff568cb138b6f05db3e140d0aa16e7d2db2
b.zubarev <b.zubarev@corp.mail.ru>
2020-05-11 22:06:49 +0300

    fix val

--- src/lightning.py
+++ src/lightning.py
@@ -94,12 +94,7 @@ class LightningDistillation(pl.LightningModule):
         binary_predictions = prediction.argmax(dim=-1)
         accuracy = (binary_predictions == targets).sum().float() / targets.shape[0]
 
-        log = {
-            'val_loss': loss.item(),
-            'val_accuracy': accuracy.item()
-        }
-
-        return {'val_loss': loss, 'log': log}
+        return {'val_loss': loss.item(), 'val_accuracy': accuracy.item()}
 
     def validation_epoch_end(self, outputs: list):
         mean_loss = torch.stack([batch['val_loss'] for batch in outputs]).mean()
@@ -125,12 +120,7 @@ class LightningDistillation(pl.LightningModule):
 
         accuracy = (prediction.argmax(dim=-1) == targets).sum().float() / targets.shape[0]
 
-        log = {
-            'test_loss': loss.item(),
-            'test_accuracy': accuracy.item()
-        }
-
-        return {'test_loss': loss, 'log': log}
+        return {'test_loss': loss.item(), 'test_accuracy': accuracy.item()}
 
     def test_epoch_end(self, outputs):
         mean_loss = torch.stack([batch['test_loss'] for batch in outputs]).mean()
